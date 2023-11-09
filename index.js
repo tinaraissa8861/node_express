478cc243f42404a93adfc0ea34f0ce3c2f05530d
Raissa <raissafabriciolima3@gmail.com>
2023-11-09 07:56:07 -0300

    fim

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const { application } = require("express")
 const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const caminhoBase = path.join(__dirname,"templates")
 
 app.get('/',(requisicao,resposta) => {
-    resposta.send("Estou funcionando com o express")
+    resposta.sendFile(`${caminhoBase}/index.html`)
+
 })
 
-app.listen(3000,()
+app.listen(3000,() => {
+    console.log("Servidor rodando na porta 3000!")
+}
 )
 
